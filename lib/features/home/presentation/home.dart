f96db5db051433370ae19913196eeac31a67b1a3
wataru endo <w4.3nd0@watarunoMacBook-Air.local>
2025-02-19 02:08:13 +0900

    add: Homeウィジェット

---
+++ lib/features/home/presentation/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Contents Lists'),
+            ElevatedButton(onPressed: () => context.push('/counter'), child: const Text('Counter'))
+          ],
+        ),
+      ),
+    );
+  }
+}
