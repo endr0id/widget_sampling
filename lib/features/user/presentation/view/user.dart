0f3a95fa75e4a7b8981f93e10cd30341b3cad3fe
wataru endo <w4.3nd0@watarunoMacBook-Air.local>
2025-02-22 15:14:03 +0900

    add: API取得確認

--- lib/features/user/presentation/view/user.dart
+++ lib/features/user/presentation/view/user.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class User extends StatelessWidget {
   const User({super.key, required this.title});
@@ -7,11 +8,22 @@ class User extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    Future<void> fetch() async {
+      final res = await http.Client().get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+      if (res.statusCode == 200) {
+        print(res.body);
+      } else {
+        throw Exception('Failed to lead user');
+      }
+    }
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
       ),
+      floatingActionButton: FloatingActionButton(onPressed: () => fetch()),
     );
   }
 }
