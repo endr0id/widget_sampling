83fe433e307fd79a0f72c672df1cb9d84304058f
wataru endo <w4.3nd0@watarunoMacBook-Air.local>
2025-02-23 19:53:52 +0900

    modify: ユースケースのファイル名とクラス名を変更

--- lib/features/todo/domain/usecases/todo_usecase.dart
+++ lib/features/todo/domain/usecases/todo_usecase.dart
@@ -1,10 +1,10 @@
 import 'package:widget_sampling/features/todo/domain/repositories/todo_repository.dart';
 import 'package:widget_sampling/features/todo/domain/entities/todo.dart';
 
-class GetTodos {
+class TodoUseCase {
   final TodoRepository repository;
 
-  GetTodos(this.repository);
+  TodoUseCase(this.repository);
 
   Future<List<Todo>> execute() {
     return repository.getTodos();
