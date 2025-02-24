d3029d31706f1bfd9e2be39653740832c4af949c
wataru endo <w4.3nd0@watarunoMacBook-Air.local>
2025-02-24 21:36:29 +0900

    add: 環境変数からホスト名を取得

--- lib/features/todo/data/datasources/todo_remote_data_source.dart
+++ lib/features/todo/data/datasources/todo_remote_data_source.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:widget_sampling/core/utils/environment.dart';
 import 'package:widget_sampling/features/todo/data/models/todo.dart';
 
 class TodoRemoteDataSource {
@@ -9,8 +10,8 @@ class TodoRemoteDataSource {
 
   // APIリクエスト実行
   Future<List<Todo>> fetchTodos() async {
-    final response = await client
-        .get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
+    final String hostName = Environment.getHostName();
+    final response = await client.get(Uri.parse('$hostName/todo'));
 
     if (response.statusCode == 200) {
       final List<dynamic> todosJson = json.decode(response.body);
